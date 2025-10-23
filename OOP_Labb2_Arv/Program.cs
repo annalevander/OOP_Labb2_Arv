446a7737b39487f088a86e53523e746f0ae90f0a
annalevander <annalevander@icloud.com>
2025-10-23 14:45:28 +0200

    Add project files.

---
+++ OOP_Labb2_Arv/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Labb2_Arv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
