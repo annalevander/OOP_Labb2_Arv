d19849cb8b08d3481e3400485fdc370103f01505
annalevander <annalevander@icloud.com>
2025-10-27 15:21:50 +0100

    created a basic version of the Boat class that inherits from the Vehicle class

---
+++ OOP_Labb2_Arv/Boat.cs
@@ -0,0 +1,17 @@
+﻿namespace OOP_Labb2_Arv
+{
+    internal class Boat : Vehicle
+    {
+        public string Sails{ get; set; } = "Using motor";
+
+        public void UseSails()
+        {
+
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine("Båten låter brrrrrrrrrrrr");
+        }
+    }
+}
