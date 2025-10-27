52adbcea86f819911d41f3b02b0cd88daa37f0d3
annalevander <annalevander@icloud.com>
2025-10-27 15:16:07 +0100

    created a basic version of a Car class that inherits from Vehicle

---
+++ OOP_Labb2_Arv/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace OOP_Labb2_Arv
+{
+    internal class Car : Vehicle
+    {
+        public string Color { get; set; } = "Red";
+
+        public void ChangeColor()
+        {
+
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine("Bilen låter bruum bruum");
+        }
+    }
+}
