9d0dd69fd6cf77ccfd9e81178d2053e422c2b4bd
annalevander <annalevander@icloud.com>
2025-10-27 15:43:29 +0100

    constructor

--- OOP_Labb2_Arv/Vehicle.cs
+++ OOP_Labb2_Arv/Vehicle.cs
@@ -4,11 +4,22 @@
     {
         public int HorsePower { get; set; }
         public int MaxSpeed { get; set; }
-        public string Brand { get; set; }
+        public string Brand { get; set; } 
         public string Model { get; set; }
         public int ProductionYear { get; set; }
         public int Worth { get; set; }
         public string TypeOfVehicle { get; set; }
+
+        public Vehicle (int HP, int maxSpeed, string brand, string model, int prodYear, int worth, string type)
+        {
+            HorsePower = HP;
+            MaxSpeed = maxSpeed;
+            Brand = brand;
+            Model = model;
+            ProductionYear = prodYear;
+            Worth = worth;
+            TypeOfVehicle = type;
+        }
 
         public void Capacity()
         {
@@ -28,6 +39,11 @@
         public void ShowAllInfo()
         {
             Console.WriteLine($"Fordon: {TypeOfVehicle}\nMärke: {Brand}\nModell: {Model}\nProduktionsår: {ProductionYear}\nHästkrafter: {HorsePower}\nMaxhastighet: {MaxSpeed}\nVärde: {Worth}\n");
+        }
+
+        public virtual void StartEngine()
+        {
+            Console.WriteLine("Fordonet låter som en motor gör");
         }
     }
 }
